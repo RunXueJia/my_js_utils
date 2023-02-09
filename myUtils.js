c2e32ad93b4c3b281e1ae4e4ec7220abeaf87187
shenpengcheng <1608454004@qq.com>
2023-02-09 14:40:57 +0800

    添加时间方法

--- myUtils.js
+++ myUtils.js
@@ -1,4 +1,5 @@
-export function toTree( //一维数组转树形
+//一维数组转树形
+export function toTree(
   arr,
   pid = 0,
   idKey = 'id',
@@ -22,8 +23,8 @@ export function toTree( //一维数组转树形
     })
   return root ? root : arr2
 }
+//树形数组转一维
 export const toOne = (
-  //树形数组转一维
   val,
   childrenKey = 'children',
   idKey = 'id',
@@ -45,8 +46,8 @@ export const toOne = (
     return a[idKey] - b[idKey]
   })
 }
+//用于数组去重 (若为元素为对象需要传入唯一值的key)
 export function noRepeat(arr, key = false) {
-  //用于数组去重 (若为元素为对象需要传入唯一值的key)
   let obj = {}
   let newarr = arr.reduce(function (sum, current) {
     if (!key) {
@@ -58,8 +59,8 @@ export function noRepeat(arr, key = false) {
   }, [])
   return newarr
 }
+//防抖
 export const debounce = (fn, delay = 500) => {
-  //防抖
   let timer = null
   return function () {
     let _this = this
@@ -71,8 +72,8 @@ export const debounce = (fn, delay = 500) => {
     }, delay)
   }
 }
+//节流
 export const throttle = (fn, delay = 500) => {
-  //节流
   let flag = false
   return function () {
     if (!flag) {
@@ -86,23 +87,55 @@ export const throttle = (fn, delay = 500) => {
     }
   }
 }
+//替换字符串
 export function replace(text, oldVal, newWord, isAll = true) {
-  //替换字符串
   return text.replace(new RegExp(oldVal, isAll ? 'g' : ''), newWord)
 }
-export function download(fliePath, fileName) {
-  //下载方法
-  const eleLink = document.createElement('a') // 新建A标签
-  eleLink.href = fliePath // 下载的路径
-  eleLink.target = '_blank'
-  eleLink.download = `${fileName}` // 设置下载的属性，可以为空
-  eleLink.style.display = 'none'
-  document.body.appendChild(eleLink)
-  eleLink.click() // 触发点击事件
-  document.body.removeChild(eleLink)
+//下载方法
+export function download(fliePath, fileName, isImg = false) {
+  if (isImg) {
+    let image = new Image()
+    image.setAttribute('crossOrigin', 'anonymous')
+    image.src = fliePath
+    image.onload = () => {
+      let canvas = document.createElement('canvas')
+      canvas.width = image.width
+      canvas.height = image.height
+      let ctx = canvas.getContext('2d')
+      ctx.drawImage(image, 0, 0, image.width, image.height)
+      // let jpeg = /(.+(?=[.jpeg]$))/
+      // let png = /(.+(?=[.png]$))/
+      // let gif = /(.+(?=[.gif]$))/
+      // let raw = /(.+(?=[.raw]$))/
+      // let jpg = /(.+(?=[.jpg]$))/
+      // let webp = /(.+(?=[.webp]$))/
+      // let bmp = /(.+(?=[.bmp]$))/
+      // let apng  = /(.+(?=[.apng ]$))/
+
+      canvas.toBlob((blob) => {
+        let href = URL.createObjectURL(blob)
+        let Link = document.createElement('a')
+        Link.download = fileName + '.png'
+        Link.href = href
+        Link.click()
+        Link.remove()
+        // 用完释放URL对象
+        URL.revokeObjectURL(url)
+      })
+    }
+  } else {
+    const eleLink = document.createElement('a') // 新建A标签
+    eleLink.href = fliePath // 下载的路径
+    eleLink.target = '_blank'
+    eleLink.download = `${fileName}` // 设置下载的属性，可以为空
+    eleLink.style.display = 'none'
+    document.body.appendChild(eleLink)
+    eleLink.click() // 触发点击事件
+    document.body.removeChild(eleLink)
+  }
 }
+//一键复制到粘贴板
 export function copyInto(val) {
-  //一键复制到粘贴板
   const input = document.createElement('textarea')
   input.style.opacity = '0'
   input.style.position = 'absolute'
@@ -112,6 +145,40 @@ export function copyInto(val) {
   document.execCommand('copy')
   document.body.removeChild(input)
 }
+//时间
+export function getTime(val) {
+  function getWeekDay(val) {
+    week = [
+      { label: 1, value: '一' },
+      { label: 2, value: '二' },
+      { label: 3, value: '三' },
+      { label: 4, value: '四' },
+      { label: 5, value: '五' },
+      { label: 6, value: '六' },
+      { label: 0, value: '日' },
+    ]
+    return week.find((item) => item.label == val).value
+  }
+  let now = val ? new Date(val) : new Date()
+  let year = now.getFullYear() //得到年份
+  let month =
+    now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1 //得到月份
+  let day = now.getDate() < 10 ? '0' + now.getDate() : now.getDate() //得到日期
+  let week = now.getDay() //得到周几
+  let hour = now.getHours() < 10 ? '0' + now.getHours() : now.getHours() //得到小时数
+  let minute = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes() //得到分钟数
+  let second = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds() //得到秒数
+  return {
+    year,
+    month,
+    day,
+    week,
+    weekCn: getWeekDay(week),
+    hour,
+    minute,
+    second,
+  }
+}
 let my_utils = {
   toTree,
   toOne,
@@ -120,6 +187,7 @@ let my_utils = {
   throttle,
   replace,
   download,
-  copyInto
+  copyInto,
+  getTime,
 }
 export default my_utils
