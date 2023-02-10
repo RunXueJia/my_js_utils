18206f3d670f7156a26231052ba4ae30e724c4c2
shenpengcheng <1608454004@qq.com>
2023-02-10 09:48:25 +0800

    创建测试模板

---
+++ my_utils/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  created () {
+    console.log(this.$utils);
+  },
+  mounted () {
+    const obj = { id: 1, name: '张飞' }
+    const obj2 = this.$utils.clone(obj)
+    obj2.name = '关羽'
+    console.log(obj, obj2);
+  },
+  computed: {
+  },
+  watch: {
+  },
+  methods: {
+
+  },
+}
+</script>
+<style lang="scss" scoped>
+</style>
